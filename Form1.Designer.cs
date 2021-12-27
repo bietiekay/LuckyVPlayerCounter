d7b13ec9e1cdd3983c3f89778a0e86be554214b3
bietiekay <bietiekay@gmail.com>
2021-12-27 16:01:54 +0100

    erste Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace LuckyVPlayerCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playercount = new System.Windows.Forms.Label();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.fontDialog2 = new System.Windows.Forms.FontDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // playercount
+            // 
+            this.playercount.Dock = System.Windows.Forms.DockStyle.Top;
+            this.playercount.Font = new System.Drawing.Font("Microsoft Sans Serif", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playercount.ForeColor = System.Drawing.Color.White;
+            this.playercount.Location = new System.Drawing.Point(0, 0);
+            this.playercount.Name = "playercount";
+            this.playercount.Size = new System.Drawing.Size(721, 281);
+            this.playercount.TabIndex = 0;
+            this.playercount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(646, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Schriftart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(12, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Hintergrund";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(721, 319);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.playercount);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LuckyV Spielerzähler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label playercount;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FontDialog fontDialog2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UpdaterService.Net;
+
+namespace LuckyVPlayerCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 5000;
+            timer1.Start();
+            timer1.Tick += Timer_Tick;
+
+            timer1.Start();
+
+            try
+            {
+                this.BackColor = Color.Black;
+                if (File.Exists("playercount.txt"))
+                {
+                    playercount.Text = File.ReadAllText("playercount.txt");
+                }
+
+                if (File.Exists("fontsetting.txt"))
+                {
+                    TypeConverter converter = TypeDescriptor.GetConverter(typeof(Font));
+                    // Load an instance of Font from a string
+                    fontDialog2.Font = (Font)converter.ConvertFromString(File.ReadAllText("fontsetting.txt"));
+                }
+
+                if (File.Exists("fontcolorsetting.txt"))
+                {
+                    playercount.ForeColor = Color.FromArgb(Convert.ToInt32(File.ReadAllText("fontcolorsetting.txt")));
+                }
+                
+
+                if (File.Exists("colorsetting.txt"))
+                {
+                    // Load an instance of Font from a string
+                    this.BackColor = Color.FromArgb(Convert.ToInt32(File.ReadAllText("colorsetting.txt")));
+                }
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            // get player count
+            // https://api.altstats.net/api/v1/server/998/
+
+            try
+            {
+                var remoteUri = new Uri("https://api.altstats.net/api/v1/server/998/");
+
+                var http = new Fetch { Retries = 5, RetrySleep = 500, Timeout = 5000 };
+                http.Load(remoteUri.AbsoluteUri);
+                if (!http.Success)
+                {
+                    return;
+                }
+                string data = Encoding.UTF8.GetString(http.ResponseData);
+
+
+                dynamic stuff = JObject.Parse(data);
+
+                
+                playercount.Text = stuff.Players.ToString();
+
+                File.WriteAllText("playercount.txt",stuff.Players.ToString());
+
+
+            } catch(Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FontDialog fontDlg = new FontDialog();
+            //fontDialog2.ShowDialog();
+            fontDialog2.ShowColor = true;
+            fontDialog2.ShowApply = true;
+            fontDialog2.ShowEffects = true;
+            fontDialog2.ShowHelp = true;
+            fontDialog2.Font = playercount.Font;
+            fontDialog2.Color = playercount.ForeColor;
+
+            if (fontDialog2.ShowDialog() != DialogResult.Cancel)
+            {
+                playercount.Font = fontDialog2.Font;
+                playercount.ForeColor = fontDialog2.Color;
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(Font));
+                // Saving Font object as a string
+                string fontString = converter.ConvertToString(fontDialog2.Font);
+                File.WriteAllText("fontsetting.txt", fontString);
+                File.WriteAllText("fontcolorsetting.txt", fontDialog2.Color.ToArgb().ToString());
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            colorDialog1.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            colorDialog1.Color = this.BackColor;
+
+            // Update the text box color if the user clicks OK 
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText("colorsetting.txt", this.BackColor.ToArgb().ToString());
+                this.BackColor = colorDialog1.Color;
+            }
+
+
+        }
+    }
+}
